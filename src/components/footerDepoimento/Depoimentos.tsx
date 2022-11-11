53224343ae7e0eb3fef2b93da779288b0fa5a963
danilordev <danilordev@gmail.com>
2022-11-11 16:04:24 -0300

    atualizaçao

--- src/components/footerDepoimento/Depoimentos.tsx
+++ src/components/footerDepoimento/Depoimentos.tsx
@@ -1,10 +1,38 @@
 import styled from "styled-components";
 
-
 const Depoimentos = () => {
-    return(
-        <div>hello world!</div>
-    )
-}
+  return (
+    <div className="container">
+      <h3>depoimentos</h3>
+
+      <div className="row">
+        
+        <div className="col">
+          <img src="" className="image"/>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+            repellendus blanditiis harum!
+          </p>
+        </div>
+
+        <div className="col">
+          <img src="" className="image"/>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+            repellendus blanditiis harum!
+          </p>
+        </div>
+
+        <div className="col">
+          <img src="" className="image"/>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+            repellendus blanditiis harum!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Depoimentos;
