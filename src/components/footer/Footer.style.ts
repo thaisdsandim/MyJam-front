e7564a0e9d313ec7688e4f7566523dac2df1544e
danilordev <danilordev@gmail.com>
2022-11-17 20:04:38 -0300

    ATT

--- src/components/footer/Footer.style.ts
+++ src/components/footer/Footer.style.ts
@@ -2,15 +2,16 @@ import styled from "styled-components";
 
 export const Barra = styled.footer`
 width: 100%;
-height: 150px;
-margin: 0 auto;
-background: #161C20;
+height: 200px;
+margin-top: 10px;
+background: #6C5ECE;
 
 ul{
     text-align: end;
     color: #fff;
+    flex: 1;
     padding: 10px;
-    padding-top: 50px;
-    padding-right: 40px;
+    padding-top: 100px;
+    padding-right:40px;
 }
 `
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,16 @@
 import ListaOrdenada from "../components/listaOrdenada/listaOrdenada";
 import Footer from "../components/footer/Footer";
 import Slider from "../components/slideDepoimento/SliderDepoimento";
+import { Container } from "../components/onboarding/Onboarding.style";
 
 const Home = () => {
   return (
     <>
+    
       <ListaOrdenada/>
       <Slider />
       <Footer />
+    
     </>
   );
 };
