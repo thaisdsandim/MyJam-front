b9f4520752029f4d69637a97162dafda415cf2c4
danilordev <danilordev@gmail.com>
2022-11-17 15:45:53 -0300

    att

--- src/components/footer/Footer.style.ts
+++ src/components/footer/Footer.style.ts
@@ -2,17 +2,15 @@ import styled from "styled-components";
 
 export const Barra = styled.footer`
 width: 100%;
-height: 234px;
-margin-top: 80px;
+height: 150px;
+margin: 0 auto;
 background: #161C20;
 
 ul{
     text-align: end;
     color: #fff;
-    flex: 1;
-    margin-top: 20px;
     padding: 10px;
-    padding-top: 150px;
-    padding-right:40px;
+    padding-top: 50px;
+    padding-right: 40px;
 }
 `
