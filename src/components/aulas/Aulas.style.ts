5cc823b464d7e7cb7ccb89aae9fe84c0a5acdaf1
Victor Hugo Guirra <victorhugo25guirra@gmail.com>
2022-11-17 18:22:17 -0300

    atualização

--- src/components/aulas/Aulas.style.ts
+++ src/components/aulas/Aulas.style.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const AulasC = styled.div`
-     
-.listas li{
+
+     text-align: center;
+
+.listas ul{
     display: flex;
     justify-content:space-between;
     align-items: center;
+    list-style: none;
 }
 `
