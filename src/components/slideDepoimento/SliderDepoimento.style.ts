250e85c0910e449e167918b972f76a4702f56f40
danilordev <danilordev@gmail.com>
2022-11-17 15:15:34 -0300

    atualizaçao

--- src/components/slideDepoimento/SliderDepoimento.style.ts
+++ src/components/slideDepoimento/SliderDepoimento.style.ts
@@ -1,28 +1,51 @@
 // Import Swiper styles
 import styled from "styled-components";
 
+// export const BackgroundDep = styled.div`
+//   background-image: url("src/assets/images/backgroundDep.png");
+//   display: block;
+//   height: 100%;
+//   width: 100%;
+//   .titulo {
+//     justify-content: space-between;
+//     align-items: center;
+//     margin-left: 40%;
+//     /* margin-top: -50px;
+//   margin-bottom: 50px; */
+//     width: 238px;
+//     height: 47px;
+//     left: 150px;
+//     top: 60px;
+
+//     font-family: "Roboto";
+//     font-style: normal;
+//     font-weight: 600;
+//     font-size: 40px;
+//     line-height: 47px;
+//     color: #f8f8f8;
+//     padding: 60px;
+//   }
+// `;
+
 export const SliderD = styled.div`
-margin: 0 auto;
+background-image: url("src/assets/images/backgroundDep.png");
+  margin: 0 auto;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: #14131b;
   width: 100%;
-  height: 500px;
-  font-size: 14px;
-  color: #000;
-  margin: 0;
-  padding: 0;
+  height: 100%;
 
   .swiper {
-    width: 100%;
-    height: 100%;
+    width: 70%;
+    height: 60%;
+    margin: 0 auto;
   }
 
   .swiper-slide {
     text-align: center;
     font-size: 30px;
-  
+
     color: #ffffff;
     display: flex;
     justify-content: center;
@@ -37,31 +60,55 @@ margin: 0 auto;
   }
 
   .fotoDepoimento {
-      display: flex;
-      justify-content: space-between;
- 
-  
-  img{
-  overflow: hidden;
-  border-radius: 50%;
-  width: 140px;
-  height: 140px;
-  margin-right: 0px;
-
-}
+    display: flex;
+    justify-content: space-between;
+    padding: 0px;
+    width: 100%;
+    height: 100%;
+    img {
+      overflow: hidden;
+      border: 3px solid #6c5ece;
+      border-radius: 50%;
+      width: 88px;
+      height: 88px;
+      margin-right: 0px;
+    }
   }
-  
 `;
 export const Informacoes = styled.div`
-margin-left: 10px;
-p{
-    color: green;
-}
-span {
-    color :red;
-}
-`
+  text-align: left;
+  margin-left: 5px;
+  padding: 10px;
+  line-height: 20px;
+  align-content: flex-start;
+  p {
+    margin-top: 1px;
+    margin-bottom: 0px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    color: #ffffff;
+  }
+  span {
+    margin-top: 0px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #6c5ece;
+  }
+`;
 export const Depoimento = styled.div`
-
-`
-
+  width: 640px;
+  height: 114px;
+  margin-bottom: 40px;
+  h3 {
+    font-family: "Roboto";
+    font-style: italic;
+    font-weight: 300;
+    font-size: 32px;
+    line-height: 38px;
+    text-align: center;
+  }
+`;
--- src/components/slideDepoimento/SliderDepoimento.tsx
+++ src/components/slideDepoimento/SliderDepoimento.tsx
@@ -8,9 +8,13 @@ import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import { Depoimento, Informacoes, SliderD } from "./SliderDepoimento.style";
 import Icon from "../../assets/images/avatarDepoimentos.png";
+import Elipse from "../../assets/images/Ellipse.png";
+import Avatar from "../../assets/images/Avatar-maker.png"
 
 const Slider = () => {
   return (
+    <>
+        {/* <div className="titulo">Depoimentos</div> */}
     <SliderD>
       <Swiper
         spaceBetween={30}
@@ -34,6 +38,7 @@ const Slider = () => {
 
         <SwiperSlide>
           <div>
+            
             <Depoimento>
               <h3>
                 “Poder aprender sem medo de errar e sem me sentir julgada foi a
@@ -43,21 +48,55 @@ const Slider = () => {
             <div className="fotoDepoimento">
               <img src= {Icon} />
               <Informacoes>
-                <p>nome</p>
-                <span>profissão</span>
+                <p>Juliana silva e silva</p>
+                <span>Mineradodora de bitcons</span>
               </Informacoes>
             </div>
           </div>
         </SwiperSlide>
-
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>
+          <div>
+            
+            <Depoimento>
+              <h3>
+                “Muito louco meu! Nunca tinha visto um jeito tao simples para tocar. Como ninguem pensou nisso antes”
+              </h3>
+            </Depoimento>
+            <div className="fotoDepoimento">
+              <img src= {Avatar} />
+              <Informacoes>
+                <p>Enzo Valentyn Mackenzie</p>
+                <span>Tradi e Faria Limer</span>
+              </Informacoes>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+          
+            <Depoimento>
+              <h3>
+                “Depois que meu amigo zeca me apresentou esse site nunca mais deixei de tocar”
+              </h3>
+            </Depoimento>
+            <div className="fotoDepoimento">
+              <img src= {Elipse} />
+              <Informacoes>
+                <p>Jose Daniel</p>
+                <span>Somelie de wisky</span>
+              </Informacoes>
+            </div>
+          </div>
+        </SwiperSlide>
+        {/* <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
         <SwiperSlide>Slide 7</SwiperSlide>
         <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
       </Swiper>
     </SliderD>
+    </>
   );
 }
 
