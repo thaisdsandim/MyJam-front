2297bc8b62320843a9347ee6e0fa0b0c1c5b72d8
Eduardo Muniz <eduardomunizdudu@gmail.com>
2022-11-18 18:47:33 -0300

    terceira section da home finalizada

--- src/components/listaOrdenada/listaOrdenada.tsx
+++ src/components/listaOrdenada/listaOrdenada.tsx
@@ -1,39 +1,54 @@
-import { Title, SectionCard } from "./listaOrdenada.style";
+import { Title, SectionCard, Container } from "./listaOrdenada.style";
 import GuitarPlayer from "/src/assets/images/home/guitarplayer.png"
 
 
 
 const ListaOrdenada = () => {
     return(
-        <div>
+        <Container>
             <Title>Por que usar o MyJam</Title>
-
             <SectionCard>
 
                 <div>
                     <img src={GuitarPlayer} />
                 </div>
 
-                <ol>
-                    <li className="numberCircle"><h3>É gratis</h3>
+                <div className="aside">
+                    <div className="list-row">
+                    <div className="numberCircle">
+                        <p><span className="number">1</span> É gratis</p>
+                        </div>
+                    </div>
                     <span>Aprenda a tocar suas músicas favoritas para se divertir sem custo. Não é teste gratis. É gratuito mesmo.</span>
-                    </li>
+                    <hr />
 
-                    <li><h3>Se quiser mais, temos</h3>
-                    <span>Se você quiser aprender toda a teoria e técnica depois da sua música favorita, temos planos pagos para que você continue evoluindo do seu jeito.</span>
-                    </li>
-
-                    <li><h3>No seu tempo</h3>
+                    <div className="list-row">
+                    <div className="numberCircle">   
+                        <p><span className="number">2</span> No seu tempo</p>
+                        </div>
+                    </div>
                     <span>Ninguém vai te pressionar a praticar horas por dia. Você aprende no seu tempo.</span>
-                    </li>
+                    <hr />
 
-                    <li><h3>Porque faz sentido</h3>
+                    <div className="list-row">
+                    <div className="numberCircle">
+                        <p><span className="number">3</span> Porque faz sentido</p>
+                        </div>
+                    </div>
                     <span>Chega daquelas aulas sobre tempos, teorias, partituras e pausas. Aprenda o que você precisa saber pra tocar a primeira música.</span>
-                    </li>
-                </ol>
+                    <hr />
+
+
+                    <div className="list-row">
+                    <div className="numberCircle">
+                        <p><span className="number">4</span> Se quiser mais, temos</p>
+                        </div>
+                    </div>
+                    <span>Se você quiser aprender toda a teoria e técnica depois da sua música favorita, temos planos pagos para que você continue evoluindo do seu jeito.</span>
+                </div>
 
             </SectionCard>
-        </div>
+        </Container>
     )
 }
 
