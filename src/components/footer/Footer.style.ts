1f3be4cf3894d2012245ad226ca337dbc3bd0d1b
danilordev <danilordev@gmail.com>
2022-11-22 09:51:25 -0300

    atualizaçao

--- src/components/footer/Footer.style.ts
+++ src/components/footer/Footer.style.ts
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
+export const BarraBlack = styled.div`
+width: 100%;
+height: 100px;
+background: #222b35;
+border: 1px solid ;
+`
+
 export const Barra = styled.footer`
 width: 100%;
 height: 100px;
-background: #6C5ECE;
+background: linear-gradient(150deg, #6610f2 , #6C5ECE 70%);
 border: 1px solid ;
 /* border-top: 34px solid #1e1e1e; */
 
