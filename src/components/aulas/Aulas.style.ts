e626dd2e0662f2b6489d17f5792f48ff692a10e5
danilordev <danilordev@gmail.com>
2022-11-22 09:21:41 -0300

    responsividade completa

--- src/components/aulas/Aulas.style.ts
+++ src/components/aulas/Aulas.style.ts
@@ -1,18 +1,28 @@
 import styled from "styled-components";
 // import background from '../../assets/images/aulas/background.jpg';
 
-export const LightBG = styled.div `
-  background: #2A3341;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  /* background-size: cover; */
-  /* display: block; */
-  height: 70%;
+export const LightBG = styled.div`
+  background: #2a3341;
+  background-size: cover;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  display: block;
+  /* height: 70%; */
+  flex-wrap: wrap;
+  @media only screen and (max-width: 1230px){
+        height: 100%;
+      
+  }
+  @media only screen and (max-width: 600px){
+        height: 80%;
   
-`
+    }
+    @media only screen and (max-width: 390px){
+        height: 100%;
+    }
+`;
 
 export const AulasC = styled.div`
-
   height: 60vh;
   width: 100%;
   padding: 0px;
@@ -30,28 +40,52 @@ export const AulasC = styled.div`
   }
 
   img {
+    margin: 0px;
+    padding: 0px;
+    /* display: flex; */
     align-items: center;
-    height: 100%;
-    width: 100%;
-    
+    height: 240px;
+    width: 240px;
+    @media only screen and (max-width: 750px){
+        height: 200px;
+        width: 200px;
+    }
+    @media only screen and (max-width: 600px){
+        height: 150px;
+        width: 150px;
+    }
+    @media only screen and (max-width: 450px){
+        height: 130px;
+        width: 130px;
+    }
   }
-
-  .listas ul {
+ 
+  ul {
+    
     display: flex;
     justify-content: space-between;
     align-items: center;
     list-style: none;
     padding: 0px;
-    margin: 50px 0px 0px 0px;
-    @media only screen and (max-width: 750px){
-      
-    }
-    
+    margin: 0px 0px 0px 0px;
+    flex-wrap: wrap;
+   
   }
-  li{
-    margin: 0px;
-    padding: 0px;
-    
-    /* border: 1px solid #6c5ece; */
+  .listas li {
+    width: 240px;
+    height: 240px;
+        border: 1px solid #6c5ece;
+        @media only screen and (max-width: 730px){
+        height: 200px;
+        width: 200px;
+    }
+    @media only screen and (max-width: 600px){
+        height: 150px;
+        width: 150px;
+    }
+    @media only screen and (max-width: 450px){
+        height: 130px;
+        width: 130px;
+    }
   }
 `;
