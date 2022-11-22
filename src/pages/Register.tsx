af79673b6eb9dd12e2146cb364da30cfe3e13d52
thaisdsandim <contatothais.sandim@gmail.com>
2022-11-22 08:02:50 -0400

    style(login/register): some code style corrections

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,7 @@
 import { BoxRegister } from '../components/login-register/BoxRegister';
 import { Return } from "../components/login-register/Return";
-import { Box2 } from "../components/login-register/Box.style"
-import "../components/login-register/login-register.css"
-
+import { Box2 } from "../components/login-register/Box.style";
+import "../components/login-register/login-register.css";
 
 export function Register() {
     return (
