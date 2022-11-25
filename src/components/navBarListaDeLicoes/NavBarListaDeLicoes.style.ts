642168cc5f11b21a4e03c730de7d2aa6bf43141a
danilordev <danilordev@gmail.com>
2022-11-25 08:05:42 -0300

    responsividade liçoes

--- src/components/navBarListaDeLicoes/NavBarListaDeLicoes.style.ts
+++ src/components/navBarListaDeLicoes/NavBarListaDeLicoes.style.ts
@@ -15,33 +15,45 @@ export const Nav = styled.header`
 `;
 
 export const MenuLink = styled.img`
-  padding: 1rem 0.7rem;
-  @media only screen and (max-width: 600px) {
+  padding: 16px 11px;
+  /* @media only screen and (max-width: 600px) {
     width: 80%;
     padding: 0rem 2rem;
-  }
+  } */
 `;
 
 export const Menu = styled.div`
+  text-align: right;
   display: flex;
   align-items: center;
   position: relative;
   margin: 0;
-  padding-right: 4rem;
+  margin-right: 40px;
+  @media only screen and (max-width: 600px) {
+    margin: 0 auto;
+    padding: 0 auto;
+  }
+
 `;
 
 export const LogoNav = styled.img`
-  padding: 1rem 11rem;
-  color: #ffffff;
-  text-decoration: none;
+  /* padding: 1rem 11rem; */
+  /* color: #ffffff; */
+  /* text-decoration: none;
   font-weight: 900;
-  font-size: 50px;
-  margin: 0;
+  font-size: 50px; */
+  
+  margin-left: 100px;
+  margin-right: 0px;
 
-  @media only screen and (max-width: 800px) {
-    text-align: center;
-    padding: 1rem 6rem;
+  @media only screen and (max-width: 650px) {
+    margin-left:30px;
+    /* padding: 1rem 6rem; */
   }
+  @media only screen and (max-width: 610px){
+    display: none;
+  };
+
 `;
 
 export const Logo = styled.a`
