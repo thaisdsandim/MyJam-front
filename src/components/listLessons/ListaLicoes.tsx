e21f737e4d8c30fa31a096b4a6157fe186e27adf
danilordev <danilordev@gmail.com>
2022-11-25 09:10:40 -0300

    atualizaçao_buttom_play

--- src/components/listLessons/ListaLicoes.tsx
+++ src/components/listLessons/ListaLicoes.tsx
@@ -1,67 +1,67 @@
 import violao from "../../assets/images/ListaDeLicoes/violao.png";
-import play from "../../assets/images/ListaDeLicoes/playBottom.png";
 import { Lessons, Enquadro } from "./ListaLicoes.style";
+import Player from "./Playlessons";
 
 const ListLessons = () => {
   return (
     <>
-    <Enquadro>
-      <Lessons>
-        <div className="Infos">
-          <h3 className="infoH">lição 1</h3>
-          
-          <div className="divViola">
-            <img className="viola" src={violao} />
-          </div>
+      <Enquadro>
+        <Lessons>
+          <div className="Infos">
+            <h3 className="infoH">lição 1</h3>
 
-          <img className="playL" src={play} />
-        </div>
-      </Lessons>
-      <Lessons>
-        <div className="Infos">
-          <h3 className="infoH">lição 2</h3>
-          
-          <div className="divViola">
-            <img className="viola" src={violao} />
-          </div>
+            <div className="divViola">
+              <img className="viola" src={violao} />
+            </div>
 
-          <img className="playL" src={play} />
-        </div>
-      </Lessons>
-      <Lessons>
-        <div className="Infos">
-          <h3 className="infoH">lição 3</h3>
-          
-          <div className="divViola">
-            <img className="viola" src={violao} />
+            <Player />
           </div>
+        </Lessons>
+        <Lessons>
+          <div className="Infos">
+            <h3 className="infoH">lição 2</h3>
 
-          <img className="playL" src={play} />
-        </div>
-      </Lessons>
-      <Lessons>
-        <div className="Infos">
-          <h3 className="infoH">lição 4</h3>
-          
-          <div className="divViola">
-            <img className="viola" src={violao} />
+            <div className="divViola">
+              <img className="viola" src={violao} />
+            </div>
+
+            <Player />
           </div>
+        </Lessons>
+        <Lessons>
+          <div className="Infos">
+            <h3 className="infoH">lição 3</h3>
 
-          <img className="playL" src={play} />
-        </div>
-      </Lessons>
-      <Lessons>
-        <div className="Infos">
-          <h3 className="infoH">lição 5</h3>
-          
-          <div className="divViola">
-            <img className="viola" src={violao} />
+            <div className="divViola">
+              <img className="viola" src={violao} />
+            </div>
+
+            <Player />
           </div>
+        </Lessons>
+        <Lessons>
+          <div className="Infos">
+            <h3 className="infoH">lição 4</h3>
 
-          <img className="playL" src={play} />
-        </div>
-      </Lessons>
-    </Enquadro>
+            <div className="divViola">
+              <img className="viola" src={violao} />
+            </div>
+
+            <Player />
+          </div>
+        </Lessons>
+        <Lessons>
+          <div className="Infos">
+            <h3 className="infoH">lição 5</h3>
+
+            <div className="divViola">
+              <img className="viola" src={violao} />
+            </div>
+
+            <Player />
+          </div>
+        </Lessons>
+      </Enquadro>
     </>
   );
 };
---
+++ src/components/listLessons/Playlessons.tsx
@@ -0,0 +1,10 @@
+import play from "../../assets/images/ListaDeLicoes/playBottom.png";
+
+
+const Player = () => {
+    return(
+<a href={"http://google.com"}><img className="playL" src={play} /></a>
+    );
+};
+
+export default Player;
