f5cd3ead02122bcca2fe339c555388da70e92afc
thaisdsandim <contatothais.sandim@gmail.com>
2022-11-26 15:16:14 -0400

    build: add redux dependencie

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import "./index.css";
 // Nao modificar sem comunicar no grupo. 
 // Mexer em ultimo caso!
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <section>
+        <RouterProvider router={router} />
+      </section>
+    </Provider>
   </React.StrictMode>
-);
+)
