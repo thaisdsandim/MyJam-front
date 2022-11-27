cb120fea6c593fafc7c7de2cfbf8baa31ff54519
Tarik Raposo Parreira <tarikraposo@gmail.com>
2022-11-27 15:53:52 -0300

    onboarding-front/back
    Inicio da integração da página de Onboarding.

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import api from "../services/api";
 import { OnBoardingFinish } from "../components/onboarding/OnboardingFinish";
 import {
   OnBoardingComponent,
@@ -10,6 +11,11 @@ import { MyJamBar } from "../components/progressBar/MyJamBar";
 export function OnBoarding() {
   const [page, setPage] = useState(0);
   const [isFinish, setIsFinish] = useState(false);
+  const [instrumentId, setInstrumentId] = useState(1);
+  const [experienceId, setExperienceId] = useState(1);
+  const [practiceId, setPracticeId] = useState(1);
+  const [styleId, setStyleId] = useState(1);
+  const [learnId, setLearnId] = useState(1);
 
   function previousPage() {
     setPage(page - 1);
@@ -26,9 +32,17 @@ export function OnBoarding() {
   }
 
   if (isFinish) {
-    return <OnBoardingFinish />;
+    return (
+      <OnBoardingFinish
+        experienceId={experienceId}
+        instrumentId={instrumentId}
+        learnId={learnId}
+        practiceId={practiceId}
+        styleId={styleId}
+      />
+    );
   }
-
+  console.log(experienceId);
   return (
     <Container className="d-flex flex-column align-items-center">
       <MyJamBar
@@ -40,7 +54,12 @@ export function OnBoarding() {
           page={page}
           onClick={nextPage}
           title="O que você quer aprender hoje?"
-          buttonTitle={["Nenhuma", "Alguma experiência"]}
+          buttons={[
+            {
+              value: 1,
+              onClick: (value: number) => setInstrumentId(value),
+            },
+          ]}
         />
       )}
 
@@ -49,7 +68,18 @@ export function OnBoarding() {
           page={page}
           onClick={nextPage}
           title="Você já tem alguma experiência com o violão? "
-          buttonTitle={["Nenhuma", "Alguma experiência"]}
+          buttons={[
+            {
+              title: "Nenhuma",
+              value: 1,
+              onClick: (value: number) => setExperienceId(value),
+            },
+            {
+              title: "Alguma experiência",
+              value: 2,
+              onClick: (value: number) => setExperienceId(value),
+            },
+          ]}
         />
       )}
 
@@ -57,12 +87,28 @@ export function OnBoarding() {
         <OnBoardingComponent
           page={page}
           onClick={nextPage}
-          title="Quantas vezes na semana você pode praticar?"
-          buttonTitle={[
-            "Uma vez na semana",
-            "Duas vezes na semana",
-            "Apenas nos fins de semana",
-            "Todos os dias",
+          title="Quanto tempo por dia você pode praticar?"
+          buttons={[
+            {
+              title: "10 minutos ao dia",
+              value: 1,
+              onClick: (value: number) => setPracticeId(value),
+            },
+            {
+              title: "20 minutos ao dia",
+              value: 2,
+              onClick: (value: number) => setPracticeId(value),
+            },
+            {
+              title: "30 minutos ao dia",
+              value: 3,
+              onClick: (value: number) => setPracticeId(value),
+            },
+            {
+              title: "1 hora por dia",
+              value: 4,
+              onClick: (value: number) => setPracticeId(value),
+            },
           ]}
         />
       )}
@@ -72,7 +118,33 @@ export function OnBoarding() {
           page={page}
           onClick={nextPage}
           title="Qual estilo musical você mais gosta?"
-          buttonTitle={["Rock", "Pop", "Música brasileira", "Jazz", "Blues"]}
+          buttons={[
+            {
+              title: "Rock",
+              value: 1,
+              onClick: (value: number) => setStyleId(value),
+            },
+            {
+              title: "Pop",
+              value: 2,
+              onClick: (value: number) => setStyleId(value),
+            },
+            {
+              title: "Música brasileira",
+              value: 3,
+              onClick: (value: number) => setStyleId(value),
+            },
+            {
+              title: "Jazz",
+              value: 4,
+              onClick: (value: number) => setStyleId(value),
+            },
+            {
+              title: "Blues",
+              value: 5,
+              onClick: (value: number) => setStyleId(value),
+            },
+          ]}
         />
       )}
 
@@ -81,11 +153,27 @@ export function OnBoarding() {
           page={page}
           onClick={nextPage}
           title="O que você quer aprender?"
-          buttonTitle={[
-            "Aprender o básico",
-            "Todas as músicas que gosto",
-            "Tocar em eventos",
-            "O máximo possível",
+          buttons={[
+            {
+              title: "Aprender o básico",
+              value: 1,
+              onClick: (value: number) => setLearnId(value),
+            },
+            {
+              title: "Todas as músicas que gosto",
+              value: 2,
+              onClick: (value: number) => setLearnId(value),
+            },
+            {
+              title: "Tocar em eventos",
+              value: 3,
+              onClick: (value: number) => setLearnId(value),
+            },
+            {
+              title: "O máximo possível",
+              value: 4,
+              onClick: (value: number) => setLearnId(value),
+            },
           ]}
         />
       )}
