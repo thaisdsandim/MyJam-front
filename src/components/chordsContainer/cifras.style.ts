b7c843e44b3a0279ffc678220c7680c6920acd23
Eduardo Muniz <eduardomunizdudu@gmail.com>
2022-11-28 01:06:32 -0300

    create: lessonsFive

---
+++ src/components/chordsContainer/cifras.style.ts
@@ -0,0 +1,15 @@
+import styled
+ from "styled-components";
+export const Acompanhamento = styled.div `
+ display: flex;
+ 
+`;
+
+export const ChordsLyrics = styled.div `
+ 
+ `;
+
+export const ChordsView = styled.div `
+
+ 
+ `
---
+++ src/components/chordsContainer/cifras.tsx
@@ -0,0 +1,18 @@
+import { ChordsLyrics, ChordsView, Acompanhamento } from "./cifras.style";
+
+const Cifras = () => {
+    return (
+        <Acompanhamento>
+            <ChordsLyrics>
+                <iframe src="file:///C:/Users/Eduardo%20(Hacker)/Desktop/test%20iframe/index.html" frameBorder="0"></iframe>
+            </ChordsLyrics>
+
+            <ChordsView>
+
+            </ChordsView>
+        </Acompanhamento>
+    )
+}
+
+export default Cifras;
+
